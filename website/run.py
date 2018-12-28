d3739edaa8a8679de4498f6b7178e78e10c25aec
Vincentius Martin <vincentiusmartin@Vincentiuss-MacBook-Pro.local>
2018-12-27 23:56:40 -0500

    remove debug from run

--- website/run.py
+++ website/run.py
@@ -3,6 +3,6 @@
 from app import app
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',use_reloader=False,debug=True) # (debug=True,host='0.0.0.0')
+    app.run(host='0.0.0.0',use_reloader=False) # (debug=True,host='0.0.0.0')
 
     #use_reloader: somehow make this file to be called twice in debug mode
