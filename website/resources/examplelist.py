673316dbeae8aa08e095af7deeb0cc3cae5aa954
Vincentius Martin <vincentiusmartin@Vincentiuss-MacBook-Pro.local>
2018-12-29 20:28:05 -0500

    more updates

--- website/resources/examplelist.py
+++ website/resources/examplelist.py
@@ -4,16 +4,22 @@ Output type: 1 for largest absolute z-score and 2 for p-val threshold
 
 examples = \
 {
-    'ICGC Breast Cancer Mutations':{
-        'inputfile':'icgc-example.tsv',
-        'tfs':['E2F1','FOXA1','MYC'],
-        'genomever':"hg19",
-        'outputtype':1
-    },
     'MAFK ChIP-seq Allele Specific Binding Variants':{
         'inputfile':'mafk-asb-example.vcf',
         'tfs':['MAFK'],
         'genomever':"hg19",
         'outputtype':2
     },
+    'ICGC Breast Cancer Mutations - Small':{
+        'inputfile':'icgc-example-short.tsv',
+        'tfs':['E2F1','FOXA1','MYC'],
+        'genomever':"hg19",
+        'outputtype':1
+    },
+    'ICGC Breast Cancer Mutations - Large':{
+        'inputfile':'icgc-example.tsv',
+        'tfs':['E2F1','FOXA1','MYC'],
+        'genomever':"hg19",
+        'outputtype':1
+    }
 }
