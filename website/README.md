cbf5a5583096c503796f87f48df015f735e05ac1
Vincentius Martin <vincentiusmartin@Vincentiuss-MacBook-Pro.local>
2019-01-09 12:26:09 -0500

    downlaod page, readme

--- website/README.md
+++ website/README.md
@@ -1,15 +1,20 @@
-# QBiC-Pred
+# QBiC-Pred (Quantitative predictions of transcription factor binding changes due to sequence variants)
 
-<h3> Quantitative Binding Change Prediction </h3>
+## To generate TF mapping:
+1. Go to `mapping_generator` folder.
+2. Open `mapping_generator.py`. Then edit path in `gene2upbm` and `tfdb`.
+3. Run `python3 mapping_generator.py`
+4. This will create `mapping_data` folder that should contain:
+   * **tflist.txt** contains all transcription factors separated by new line.
+   * **pbmlist.txt** contains all pbm filenames used by QBiC.
+   * **pbmtohugo.txt** contains mapping from pbm to hgnc name.
+       Each line contains mapping in the format of `pbmname:tf1,tf2,...`.
+   * **hugotopbm.txt** contains mapping from hgnc name to pbm.
+       Each line contains mapping in the format of
+       `familyname->tf1:pbm1,pbm2,...;tf2:pbm1,pbm2,...`
 
-<h4>To generate TF mapping:</h4>
-<ul>
-  <li>Edit mapping_generator.py and input tflist. This will be a part of the
-  running command after the mapping method is finished.</li>
-  <li>Run mapping_generator.py</li>
-</ul>
-
-<h4>To run the webserver:</h4>
-<ul>
-  <li>Install nginx: http://www.leeladharan.com/installing-nginx-flask-uwsgi-on-mac-osx-yosemite</li>
-</ul>
+## Roadmap
+- [x] roadmap1
+- [x] roadmap2
+- [x] roadmap3
+- [ ] roadmap4
---
+++ website/mapping_generator/mapping_generator.py
@@ -0,0 +1,59 @@
+import os
+import pandas as pd
+import shutil
+
+def dbd2hugo2pbm(gene_dict,tfdb_df):
+    dbd_mapping = {}
+
+    for gene in gene_dict.keys():
+        dbds = tfdb_df[tfdb_df["HGNC symbol"]==gene]["DBD"].unique().tolist()
+        for dbd in dbds: # a TF shouldn't have >1 DBD, but just in case
+            if dbd in dbd_mapping:
+                dbd_mapping[dbd].append("%s:%s"%(gene,",".join(gene_dict[gene])))
+            else:
+                dbd_mapping[dbd] = ["%s:%s"%(gene,",".join(gene_dict[gene]))]
+    with open("mapping_data/hugotopbm.txt",'w') as f:
+        dbds = sorted(dbd_mapping.keys(), key=lambda s: s.lower())
+        towrite = ""
+        for dbd in dbds:
+            liststr = ";".join(dbd_mapping[dbd])
+            towrite += ("%s->%s\n" % (dbd,liststr))
+        f.write(towrite.strip())
+
+def pbm2hugo(gene_dict):
+    pbm_dict = {}
+    for gene in gene_dict.keys():
+        for tf in gene_dict[gene]:
+            if tf in pbm_dict and gene not in pbm_dict[tf]:
+                pbm_dict[tf].append(gene)
+            else:
+                pbm_dict[tf] = [gene]
+    pbms = sorted(pbm_dict.keys(), key=lambda s: s.lower())
+    with open("mapping_data/tflist.txt",'w') as f:
+        f.write("\n".join(gene_dict.keys()))
+    with open('mapping_data/pbmlist.txt','w') as f:
+        f.write("\n".join(pbms))
+    with open("mapping_data/pbmtohugo.txt",'w') as f:
+        towrite = ""
+        for pbm in pbms:
+            towrite += "%s:%s\n"%(pbm,",".join(pbm_dict[pbm]))
+        f.write(towrite.strip())
+
+def generate_mapping_webserv(infile,tfdb):
+    gene_df = pd.read_csv(gene2upbm)
+    gene_dict = dict(zip(gene_df['gene'], gene_df['upbm']))
+    gene_dict = {gene:[os.path.splitext(tf)[0].strip() for tf in gene_dict[gene].split(";")] for gene in gene_dict.keys()}
+    tfdb_df = pd.read_csv(tfdb)
+
+    # Create a directory to write the mapping results
+    if os.path.exists("mapping_data") and os.path.isdir("mapping_data"):
+        shutil.rmtree("mapping_data")
+    os.mkdir("mapping_data")
+
+    dbd2hugo2pbm(gene_dict,tfdb_df)
+    pbm2hugo(gene_dict)
+
+if __name__ == "__main__":
+    gene2upbm = "../app/static/files/mapping_gene_to_upbm_final.csv"
+    tfdb = "resources_from_humantfs/DatabaseExtract_v_1.01.csv"
+    generate_mapping_webserv(gene2upbm,tfdb)
