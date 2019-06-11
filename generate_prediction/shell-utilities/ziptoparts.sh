0125435240f17593ad228bdbc033167543862589
Vincentius Martin <vincentiusmartin@Vincentiuss-MacBook-Pro.local>
2019-06-11 15:09:00 -0400

    zipper

--- generate_prediction/shell-utilities/ziptoparts.sh
+++ generate_prediction/shell-utilities/ziptoparts.sh
@@ -2,7 +2,7 @@
 
 dirname=tozip
 zipname=xxx
-splitsize=5
+splitsize=2
 
 if ! [ -d $dirname ]; then
     echo "couldn't find the directory"
@@ -16,19 +16,31 @@ splitidx=$(( (numfiles + 1) / splitsize ))
 i=0
 partnum=1
 files=""
+finfo=""
 for content in *
 do
     i=$((i+1))
     files="$files $content"
     if  [ "$i" -ne 0 ] && [ $(( $i % $splitidx )) -eq 0 ]; then
         zip  ${curdir}/${zipname}_${partnum}.zip $files
-        partnum=$((partnum+1))
+        # write zip info to file
+        flist=$( echo $files | tr ' ' '\n' )
+        finfo="${finfo}Zipname: ${zipname}_${partnum}.zip\n$flist\n\n"
+        # reset files string and update partnum
         files=""
+        partnum=$((partnum+1))
     fi
 done
+
+# for leftovers
 if [ "$files" != "" ]; then
     zip  ${curdir}/${zipname}_${partnum}.zip $files
+    flist=$( echo $files | tr ' ' '\n' )
+    finfo="${finfo}Zipname: ${zipname}_${partnum}.zip\n$flist"
 fi
+
+rm -f "${curdir}/zipinfo.txt"
+printf "$finfo" >> "${curdir}/zipinfo.txt"
 
 
 #zip  ${curdir}/${zipname}.zip * -s ${splitsize}
