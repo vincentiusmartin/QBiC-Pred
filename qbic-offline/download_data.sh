121d01139e05851182ac565f1924355f49e5504d
isrobson <isrobson@email.unc.edu>
2019-10-03 00:29:35 -0400

    add unzip

--- qbic-offline/download_data.sh
+++ qbic-offline/download_data.sh
@@ -12,6 +12,8 @@ wget -N http://qbic.genome.duke.edu/download/escore.zip
 unzip escore
 mkdir -p chromosomes
 cd chromosomes
+mkdir -p hg19
+cd hg19
 
 # chromosomes -- just hg19
 rsync -avzP --ignore-existing rsync://hgdownload.cse.ucsc.edu/goldenPath/hg19/chromosomes/ .
@@ -30,4 +32,6 @@ for i in 1 2 3 4 5 6 7 8 9 10
 do
   wget -N http://qbic.genome.duke.edu/download/predpval/predpval_$i.zip
 done
+
+unzip '*.zip'
 
