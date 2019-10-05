2878600c5d19f28f7f819c8399272b4553e9f510
isrobson <isrobson@email.unc.edu>
2019-10-05 16:16:04 -0400

    convert back to cProfile

--- qbic-offline/cprofile.sh
+++ qbic-offline/cprofile.sh
@@ -1,2 +1,2 @@
-python -m cProfile -o profile.stats qbic.py -i testing_resources/input_mutation_test.vcf -g testing_resources/tflist_test.txt -c hg19 -o predictons.tsv -v 0.50
+python -m cProfile -o profile.stats qbic.py -i testing_resources/input_mutation_test.vcf -g testing_resources/tflist_test.txt -c hg19 -o predictons.tsv -v 0.50 -n 2
 
